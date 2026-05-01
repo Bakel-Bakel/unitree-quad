143b3be088ad88418c289f316130f0d8588f5a70
bakel-bakel <begededum4bakel@gmail.com>
2026-05-01 11:43:05 +0200

    take2

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-import time
-from unitree_sdk2py.core.channel import ChannelFactory
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize  # <-- change this import
 from unitree_sdk2py.go2.sport.sport_client import SportClient
+import time
 
-ChannelFactory.Instance().Init(0, "wlan0")
+ChannelFactoryInitialize(0, "wlan0")  # <-- and change this line
 
 client = SportClient()
 client.SetTimeout(10.0)
 client.Init()
 
 client.StandUp()
-time.sleep(2)  # wait for it to fully stand before moving
+time.sleep(2)
 
-client.Move(0.3, 0, 0)  # move forward slowly at 0.3 m/s
-time.sleep(1)           # move for 1 second (~0.3 metres)
-client.StopMove()       # stop
+client.Move(0.3, 0, 0)
+time.sleep(1)
+client.StopMove()
