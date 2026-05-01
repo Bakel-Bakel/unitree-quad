86802aff5f0c4c66e9154ebbf567810ac0f561e3
bakel-bakel <begededum4bakel@gmail.com>
2026-05-01 12:37:11 +0200

    updated go to unitree

--- test2.py
+++ test2.py
@@ -1,25 +1,36 @@
 import asyncio
-from go2_webrtc_driver.webrtc_driver import Go2WebRTCConnection, WebRTCConnectionMethod
-from go2_webrtc_driver.constants import RTC_TOPIC
+from unitree_webrtc_connect import UnitreeWebRTCConnection, WebRTCConnectionMethod
 
 async def main():
-    conn = Go2WebRTCConnection(
-        WebRTCConnectionMethod.LocalSTA,  # STA mode = both on same router
-        ip="192.168.1.70"                 # Go2's IP
+    # Both laptop and Go2 are on the same router (192.168.1.x)
+    conn = UnitreeWebRTCConnection(
+        WebRTCConnectionMethod.LocalSTA,
+        ip="192.168.1.70"  # your Go2's IP
     )
     await conn.connect()
+    print("Connected!")
 
-    # Move forward
+    # Stand up
     await conn.datachannel.pub_sub.publish_request_new(
-        RTC_TOPIC["SPORT_MOD"],
-        {"api_id": 1008, "parameter": {"x": 0.3, "y": 0, "z": 0}}
+        "rt/api/sport/request",
+        {"header": {"identity": {"id": 1, "api_id": 1004}}, "parameter": "{}"}
+    )
+    await asyncio.sleep(2)
+
+    # Move forward slowly
+    await conn.datachannel.pub_sub.publish_request_new(
+        "rt/api/sport/request",
+        {"header": {"identity": {"id": 2, "api_id": 1008}},
+         "parameter": '{"x": 0.3, "y": 0, "z": 0}'}
     )
     await asyncio.sleep(1)
 
     # Stop
     await conn.datachannel.pub_sub.publish_request_new(
-        RTC_TOPIC["SPORT_MOD"],
-        {"api_id": 1008, "parameter": {"x": 0, "y": 0, "z": 0}}
+        "rt/api/sport/request",
+        {"header": {"identity": {"id": 3, "api_id": 1008}},
+         "parameter": '{"x": 0, "y": 0, "z": 0}'}
     )
+    print("Done!")
 
 asyncio.run(main())
